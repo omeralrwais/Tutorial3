2d7091f1463b77f0c9794850d0d9b60b089451da
Omer Alrwais <omer.alrwais@cgu.edu>
2013-05-31 01:33:38 -0700

    Added a Back button to 2nd screen

--- Tutorial3/src/cgu/edu/ist380/tutorial3/Display.java
+++ Tutorial3/src/cgu/edu/ist380/tutorial3/Display.java
@@ -1,9 +1,21 @@
 package cgu.edu.ist380.tutorial3;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Display extends Activity{
@@ -13,14 +25,32 @@ public class Display extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display); {
       	
+        	
         	Intent i = getIntent();
             Bundle b = i.getExtras();
             String ageText = b.getString("Age");
          
             TextView title = (TextView) findViewById(R.id.age);
             title.setText("my age is: " + ageText);  
+            
+            Button mainNext = (Button) findViewById(R.id.back);
+            
+            mainNext.setOnClickListener(new OnClickListener() {
+                public void onClick(View v) {
+                
+                    Intent i = new Intent();
+                    i.setClassName("cgu.edu.ist380.tutorial3", "cgu.edu.ist380.tutorial3.MainActivity");
+                    startActivity(i);
+                    
+             
+                }
+            });
+            
+                	
+        }    
+
        
-        }
+     
     }
     
 
